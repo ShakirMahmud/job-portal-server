639096d11d46a89b07c9220438dff597f405a6fd
ShakirMahmud <shakirmahmud50@gmail.com>
2024-12-15 23:23:04 +0600

    delete

--- index.js
+++ index.js
@@ -126,6 +126,14 @@ async function run() {
       res.send(application);
     });
 
+    // delete application
+    app.delete('/applications/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await applicationCollection.deleteOne(query);
+      res.send(result);
+    });
+
 
 
   } finally {
