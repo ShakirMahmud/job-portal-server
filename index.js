64a94b0812bcc9a03cfb2a3eb8ce1ca2c87c12f9
ShakirMahmud <shakirmahmud50@gmail.com>
2024-12-20 00:04:44 +0600

    handle logout to remove token from the cookies

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const port = process.env.PORT || 5000;
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173",
+      "https://job-portal-shakir.vercel.app/",
+      "https://job-portal-server-shakir.vercel.app/"
+    ],
     credentials: true,
   })
 );
@@ -46,12 +49,12 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
 
     // create job connection
     const jobCollection = client.db("job-portal").collection("jobs");
